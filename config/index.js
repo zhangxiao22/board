fa3efb6d7af87ca5432adb1673911a40d321fd23
张骁 <anyishuo@126.com>
2019-06-13 11:30:38 +0800

    跨域

--- config/index.js
+++ config/index.js
@@ -11,13 +11,13 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': {
-        target: 'http://api.stramogroup.com/',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
+      // '/api': {
+      //   target: 'http://api.stramogroup.com/',
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     '^/api': ''
+      //   }
+      // }
 
     },
 
