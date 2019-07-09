9747b8ff4fd285bde355306fca7491fe25d87c20
张骁 <anyishuo@126.com>
2019-07-09 11:17:56 +0800

    ?

--- src/views/board/radar.vue
+++ src/views/board/radar.vue
@@ -120,9 +120,9 @@
       getChart1() {
         return new Promise(resolve => {
           equipments({department: this.name1}).then(res => {
-            this.chart1.data = res.data.map(n => {
+            this.chart1.data = res.data.map((n, i) => {
               return Object.assign({}, n, {
-                name: n.name + ' ' + n.seriesNumber,
+                name: n.name + ' ' + n.brand + '' + n.model + '?' + i,
                 '收入': n.income,
                 '支出': n.expense,
               })
@@ -220,7 +220,7 @@
               }
             })
             return `<div class="g2-tooltip">
-                    <div class="g2-tooltip-title">${title}</div>
+                    <div class="g2-tooltip-title">${title.split('?')[0]}</div>
                     <ul class="g2-tooltip-list">
                       <li class="g2-tooltip-li">设备价值：${_this.numberComma(data.value)}万</li>
                       <li class="g2-tooltip-li">型号：${_this.numberComma(data.model)}</li>
